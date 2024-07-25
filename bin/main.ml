6c36426133896d8264a383da3f6808fc21e28a90
Praveen Thomas <thomasmethrayil@gmail.com>
2024-07-26 02:53:11 +0530

    Show prompt in case of empty file

--- bin/main.ml
+++ bin/main.ml
@@ -17,33 +17,42 @@ let () =
         map
           (anon ("filename" %: string))
           ~f:(fun filename () ->
-            let parse_and_solve ~pool =
-              let lines =
-                In_channel.read_lines filename |> List.mapi ~f:(fun i p -> p, Some i)
+            let lines =
+              In_channel.read_lines filename |> List.mapi ~f:(fun i p -> p, Some i)
+            in
+            if List.length lines > 0
+            then (
+              let parse_and_solve ~pool =
+                Executor_pool.submit_exn pool ~weight:1.0 (fun () ->
+                  Fiber.List.iter
+                    (fun (p, i_opt) ->
+                      let open Solver in
+                      load_puzzle_and_solve p i_opt)
+                    lines)
               in
-              Executor_pool.submit_exn pool ~weight:1.0 (fun () ->
-                Fiber.List.iter
-                  (fun (p, i_opt) ->
-                    let open Solver in
-                    load_puzzle_and_solve p i_opt)
-                  lines)
-            in
-            let num_of_threads =
-              match Sys.getenv "num_threads" with
-              | Some num -> Int.of_string num
-              | None -> Domain.recommended_domain_count ()
-            in
-            Eio_main.run
-            @@ fun env ->
-            Switch.run
-            @@ fun sw ->
-            let pool =
-              Executor_pool.create
-                ~sw
-                (Stdenv.domain_mgr env)
-                ~domain_count:num_of_threads
-            in
-            parse_and_solve ~pool))
+              let num_of_threads =
+                match Sys.getenv "num_threads" with
+                | Some num -> Int.of_string num
+                | None -> Domain.recommended_domain_count ()
+              in
+              Eio_main.run
+              @@ fun env ->
+              Switch.run
+              @@ fun sw ->
+              let pool =
+                Executor_pool.create
+                  ~sw
+                  (Stdenv.domain_mgr env)
+                  ~domain_count:num_of_threads
+              in
+              parse_and_solve ~pool)
+            else printf "The input file %s: does not contain any puzzles" filename))
   in
-  Command_unix.run ~version:"1.0" ~build_info:"RWO" command
+  let time f =
+    let start = Unix.gettimeofday () in
+    let stop = Unix.gettimeofday () in
+    let () = printf "Execution time: %fs\n%!" (stop -. start) in
+    f
+  in
+  Command_unix.run ~version:"1.0" ~build_info:"RWO" (time command)
 ;;
